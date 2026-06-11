a89b0707012a9fe12f67ead9157df09b26d37caa
DESKTOP-PLMAB5D\이효주 <julie08051@naver.com>
2026-06-11 17:28:34 +0900

    Overwrite revival machine with sub-altar state

--- sub-altar.h
+++ sub-altar.h
@@ -17,7 +17,7 @@ HAS2_Wifi has2wifi("http://172.30.1.43");
 SecureOTA ota(
   "https://raw.githubusercontent.com/Fuzzyline-HAS2/sub-altar/main/update.bin",
   "https://raw.githubusercontent.com/Fuzzyline-HAS2/sub-altar/main/version.txt",
-  "https://raw.githubusercontent.com/Fuzzyline-HAS2/sub-altar/main/update.sig",
+  "https://raw.githubusercontent.com/Fuzzyline-HAS2/sub-altar /main/update.sig",
   HMAC_SECRET,
   FIRMWARE_VER
 );
